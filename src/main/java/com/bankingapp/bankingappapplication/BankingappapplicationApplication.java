79e96545b8077a8b923c1932ef713461cff01247
B4khytu1y <bakhytulynurzhan@gmail.com>
2024-10-10 16:36:48 +0500

    first commit

---
+++ src/main/java/com/bankingapp/bankingappapplication/BankingappapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapp.bankingappapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingappapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingappapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankingapp/bankingappapplication/BankingappapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapp.bankingappapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingappapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
