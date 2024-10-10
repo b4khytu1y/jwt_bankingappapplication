0f32d12d60e771f1fa328938d200563ac4703350
B4khytu1y <bakhytulynurzhan@gmail.com>
2024-10-10 23:42:49 +0500

    changes

--- src/main/resources/bd/data.sql
+++ src/main/resources/bd/data.sql
@@ -1,14 +1,13 @@
--- CREATE TABLE IF NOT EXISTS users (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(50) NOT NULL UNIQUE,
---     password VARCHAR(100) NOT NULL
--- );
-
--- CREATE TABLE IF NOT EXISTS accounts (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     account_number VARCHAR(20) NOT NULL UNIQUE,
---     balance DECIMAL(19, 2) NOT NULL,
---     account_type VARCHAR(20) NOT NULL,
---     user_id BIGINT,
---     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- );
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
+CREATE TABLE accounts (
+    id BIGINT PRIMARY KEY,
+    account_number VARCHAR(20) NOT NULL UNIQUE,
+    balance DECIMAL(19, 2) NOT NULL,
+    account_type VARCHAR(20) NOT NULL,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
